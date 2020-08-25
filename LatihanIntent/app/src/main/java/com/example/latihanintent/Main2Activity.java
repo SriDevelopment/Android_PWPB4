3f6c1f3521a0a42af02749e89b9129ef82281903
Sri_Nuraeni <60680165+SriDevelopment@users.noreply.github.com>
2020-08-25 20:16:15 +0700

    Tugas PWPB ke 4

---
+++ LatihanIntent/app/src/main/java/com/example/latihanintent/Main2Activity.java
@@ -0,0 +1,20 @@
+package com.example.latihanintent;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.security.Key;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        TextView t = findViewById(R.id.text_viewdata);
+        Bundle bundle = getIntent().getExtras();
+        String s = bundle.getString(Key:"name");
+        t.setText(s);
+    }
+}
